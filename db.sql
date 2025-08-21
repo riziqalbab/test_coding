efde940b49424b0b2bcd1accf2e3f457c81affe5
riziqalbab <albabriziq36@gmail.com>
2025-08-21 11:39:22 +0700

    db

--- db.sql
+++ db.sql
@@ -2,7 +2,6 @@
 CREATE DATABASE IF NOT EXISTS learn_yii;
 USE learn_yii;
 
--- Account table for users
 CREATE TABLE IF NOT EXISTS `account` (
     `username` VARCHAR(45) NOT NULL,
     `password` VARCHAR(250) NOT NULL,
@@ -11,7 +10,6 @@ CREATE TABLE IF NOT EXISTS `account` (
     PRIMARY KEY (`username`)
 ) ENGINE = InnoDB;
 
--- Post table
 CREATE TABLE IF NOT EXISTS `post` (
     `idpost` INT NOT NULL AUTO_INCREMENT,
     `title` TEXT NOT NULL,
@@ -27,9 +25,6 @@ CREATE TABLE IF NOT EXISTS `post` (
         ON UPDATE NO ACTION
 ) ENGINE = InnoDB;
 
--- Insert dummy users
 INSERT INTO account (username, password, name, role) VALUES 
 ('admin', '$2y$10$UQz7fOiZWwM0CQYt2UTBWewDapV8ZxgsQ39/boEID62.xS10LfFGi', 'Administrator', 'admin'),
 ('author', '$2y$10$p2M7T2MbBtYQdCLAX7DvpO/czKEZDA8DDZWNaIKcqCNCsCvUq/NM2', 'Author User', 'author');
-
--- Note: Password for admin is 'admin' and for author is 'author'
